f4e3efac5d8141be32d24d6c884ee0666698aa08
myoimaty <mati.dominguez@duocuc.cl>
2023-08-16 11:58:26 -0400

    primera venida

---
+++ src/app/pages/jugadores/jugadores.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-jugadores',
+  templateUrl: './jugadores.page.html',
+  styleUrls: ['./jugadores.page.scss'],
+})
+export class JugadoresPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+
+
+}
